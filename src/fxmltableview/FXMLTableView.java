e15aed58d872d1ce1a190ee1eeab0891a2a2966a
Ivan <Ivan@DESKTOP-63FDDDU>
2022-10-31 10:27:46 +0100

    Programa acabado

---
+++ src/fxmltableview/FXMLTableView.java
@@ -0,0 +1,30 @@
+package fxmltableview;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Ivan
+ */
+public class FXMLTableView extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+       primaryStage.setTitle("FXML TableView Example");
+       
+       //Reference to FXML
+       Pane myPane = (Pane)FXMLLoader.load(getClass().getResource("fxml_tableview.fxml"));
+       
+       Scene myScene = new Scene(myPane);
+       primaryStage.setScene(myScene);
+       primaryStage.show();
+    }
+ 
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
